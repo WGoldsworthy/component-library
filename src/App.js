b8ff3ea1fe23c9861657466e01a7755503654dde
William Goldsworthy <william.goldsworthy@tiscali.co.uk>
2019-12-09 09:00:53 +0000

    Card Carousel

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MainHeader from './components/molecules/MainHeader';
 import MenuButton from './components/organisms/MenuButton';
 import AreaTag from './components/organisms/AreaTag';
 import RedBackText from './components/atoms/RedBackText';
+import CardCarousel from './components/CardCarousel';
 import ScreenAreaWrapper from './components/organisms/ScreenAreaWrapper'
 import './App.css';
 
@@ -13,23 +14,29 @@ function App() {
 
   return (
     <div className="App">
-    	<Container>
-	    	<ScreenAreaWrapper>
-	    		<MenuButton />
-	    		<MainHeader/>
-	    		<AreaTag />
-	    	</ScreenAreaWrapper>
-	    	<Row className="section__about">
-	    		<Col xs={6}>
-	    			<ReactLogoAnimation />
-	    		</Col>
-	    		<Col xs={5} className="section__about__info">
-	    			My name is Will. I am a <RedBackText>software developer</RedBackText> currently working
-	    			in London. I mostly work on <RedBackText>front end React code</RedBackText> but also get involved 
-	    			with plenty of <RedBackText>node.js and python</RedBackText> too when I can!
-	    		</Col>
-	    	</Row>
-    	</Container>
+    	<ScreenAreaWrapper>
+    		<MenuButton />
+    		<MainHeader/>
+    		<AreaTag />
+    	</ScreenAreaWrapper>
+    	<Row className="section__about">
+    		<Col xs={6}>
+    			<ReactLogoAnimation />
+    		</Col>
+    		<Col xs={5} className="section__about__info">
+    			My name is Will. I am a <RedBackText>software developer</RedBackText> currently working
+    			in London. I mostly work on <RedBackText>front end React code</RedBackText> but also get involved 
+    			with plenty of <RedBackText>node.js and python</RedBackText> too when I can!
+    		</Col>
+    	</Row>
+    	<Row className="section__work">
+    		<Col xs={5}>
+    			I have worked for a range of companies and clients during my time in the software industry.
+    		</Col>
+    		<Col>
+    			<CardCarousel />
+    		</Col>
+    	</Row>
     </div>
   );
 }
